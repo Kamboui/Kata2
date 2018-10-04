f114129a28a0d22c659eca49adfee21078ba89b2
Usuario <Kamboui>
2018-10-04 11:57:57 +0100

    Kata2 version 2

---
+++ src/kata2/Histogram.java
@@ -0,0 +1,31 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Histogram {
+    private final int[] data;
+
+    public Histogram(int[] data) {
+        this.data = data;
+    }
+
+    public int[] getData() {
+        return data;
+    }
+    
+    public Map getHistogram(){
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        
+        for(int i : data){
+            try{
+                histogram.put(data[i], histogram.get(data[i])+1);
+            } catch(Exception e){
+                System.out.println("Clave \"" + data[i] + "\" no encontrada.... Añadiendo nueva clave");
+                histogram.put(data[i], 1);
+            }
+        }
+        return histogram;
+    }
+    
+}
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,18 +7,12 @@ public class Kata2 {
     public static void main(String[] args) {
         // TODO code application logic here
         int data[] = {4,5,6,2,3,5,4,6,2,3,5,4,6,6,6};
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        for(int i : data){
-            try{
-                histogram.put(data[i], histogram.get(data[i])+1);
-            } catch(Exception e){
-                System.out.println("Clave \"" + data[i] + "\" no encontrada.... Añadiendo nueva clave");
-                histogram.put(data[i], 1);
-            }
-        }
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        
         System.out.println("\nContenido del mapa (Clave ==> Valor)");
-        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
             System.out.println( entry.getKey() + " ===> " + entry.getValue());
         } 
     }
