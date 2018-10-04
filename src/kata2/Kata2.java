f25aaa1a3d45ee79dca838858db297b68832f347
Usuario <Kamboui>
2018-10-04 12:22:36 +0100

    Kata2 version 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,11 +6,11 @@ import java.util.Map;
 public class Kata2 {
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] data = {4,5,6,2,3,5,4,6,2,3,5,4,6,6,6};
+        String[] data = {"salmon", "papas", "aguacate", "papas", "microondas", "salmon", "aguacate", "aguacate", "salmon"};
         Histogram histo = new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
+        Map<String,Integer> histogr = histo.getHistogram();
         System.out.println("\nContenido del mapa (Clave ==> Valor)");
-        for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
+        for (Map.Entry<String, Integer> entry : histogr.entrySet()) {
             System.out.println( entry.getKey() + " ===> " + entry.getValue());
         } 
     }
